d3e058cb189156235aff5838fbc6d9c025208896
dheeraj0808 <dheerajsingh1939@gmail.com>
2026-02-21 21:36:06 +0530

    db connection is finally successfully completed

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
+require('dotenv').config();
 const app = require("./src/app");
+const pool = require('./src/db/db');
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-});
+// Code shuru, pehle database check karo:
+pool.getConnection()
+    .then(() => {
+        // Agar yaha tak aaya matlab connected!
+        console.log("Database se connect ho gaya!");
+
+        // Ab hum apna node.js/express server start kar sakte hain
+        app.listen(PORT, () => {
+            console.log(`Server chal raha hai (Port ${PORT})`);
+        });
+    })
+    .catch((err) => {
+        // Agar database nai chala to ye error aayega
+        console.log("Database Error:", err);
+    });
