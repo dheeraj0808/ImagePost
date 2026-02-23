75b4cd1d94ec592e46252fa74a5f83c2e293d11d
dheeraj0808 <dheerajsingh1939@gmail.com>
2026-02-24 00:05:25 +0530

    create a feed page

---
+++ frontend/src/Pages/Feed.jsx
@@ -0,0 +1,11 @@
+import React , {useState , useEffect}from 'react'
+
+const Feed = () => {
+  return (
+    <section className='feed-section'>
+        <h1>Feed</h1>
+    </section>
+  )
+}
+
+export default Feed
