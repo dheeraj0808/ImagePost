3379b5ed2a52bce5e190d345c650dbe1479e040b
dheeraj0808 <dheerajsingh1939@gmail.com>
2026-02-24 00:06:30 +0530

    changes in app.jsx to improving the ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom'
+import CreatePost from './Pages/CreatePost'
+import Feed from './Pages/Feed'
+import './index.css'
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      {/* Navbar */}
+      <nav className="navbar">
+        <div className="nav-logo">📸 ImagePost</div>
+        <div className="nav-links">
+          <NavLink to="/" className="nav-link">Feed</NavLink>
+          <NavLink to="/create" className="nav-link">Create Post</NavLink>
+        </div>
+      </nav>
+
+      {/* Pages */}
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/create" element={<CreatePost />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   )
 }
 
